5491dd7c5a8e7eb2059b4321232ee2aa1211d044
isama22 <elisabethmartinez@Elisabeths-MacBook-Pro.local>
2020-06-02 15:40:25 -0700

    Updates

---
+++ precache-manifest.d9d0d3dd91a04a6f816eeb6f42618d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564cc1147ad77f47df5c2ecc0c166015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba97c9b6279ef464d63",
+    "url": "/static/css/main.2ddb3bd6.chunk.css"
+  },
+  {
+    "revision": "9112831ef282bc6034d7",
+    "url": "/static/js/2.3a359936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a359936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba97c9b6279ef464d63",
+    "url": "/static/js/main.09acd39f.chunk.js"
+  },
+  {
+    "revision": "39b4e3437412f05344f7",
+    "url": "/static/js/runtime-main.cdf7684c.js"
+  }
+]);
